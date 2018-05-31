1c13ccf90c026a5d5a394c04283fcb09ee71ac5c
francisco <jhersonhuacho@gmail.com>
2018-05-31 10:11:57 -0500

    menì offcanvas con foundation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function(event){
+  const PANEL = $(this).data('toggleDia');
+  console.log(PANEL);
+  $('#lineup-tabs').foundation('selectTab', PANEL);
+});
+
+const $offCanvas = $('#offCanvas');
+
+$offCanvas.find('li').click(function (event) {
+  $offCanvas.foundation('close');
+});
